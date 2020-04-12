70a3bc9328aebf6931140c58283ca4b546023366
Hallo <Hallo@DESKTOP-GAS2A9T>
2020-04-12 20:25:59 +0200

    Tarea 1

---
+++ src/tarea1/Tarea1.java
@@ -0,0 +1,19 @@
+package tarea1;
+
+/**
+ *
+ * @author ivansteuerberg
+ */
+public class Tarea1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        Componentes vis = new Componentes();
+        
+    }
+    
+}
